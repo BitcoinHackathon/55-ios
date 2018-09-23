794898c39730a27d6879cfd76286b5aa2b24cb66
zizi4n5 <zizi4n5@gmail.com>
2018-09-23 10:31:26 +0900

    fix: 位置情報取得で落ちる問題を修正しました。

--- team-d-55-ios/UserViewController.swift
+++ team-d-55-ios/UserViewController.swift
@@ -18,55 +18,66 @@ class UserViewController: UIViewController, MKMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let sourceLocation = CLLocationCoordinate2D(latitude:39.173209 , longitude: -94.593933)
-        let sourceLocation = SwiftLocation.Locator.currentLocation!.coordinate
-        
-        let targetAddress = UserDefaults.standard.string(forKey: "targetAddress") ?? "渋谷駅"
-        SwiftLocation.Locator.location(
-            fromAddress: targetAddress,
+        Locator.currentPosition(
+            accuracy: .house,
+            timeout: Timeout.delayed(60.0),
             onSuccess: { cllocation in
                 print("Location:\(cllocation.debugDescription)")
                 
-//                let destinationLocation = CLLocationCoordinate2D(latitude:38.643172 , longitude: -90.177429)
-                guard let destinationLocation = cllocation.first?.coordinates else {
-                    print("targetAddress not found")
-                    return
-                }
-
-//                let sourcePin = CustomPin(pinTitle: "現在地", pinSubTitle: "", location: sourceLocation)
-//                self.mapView.addAnnotation(sourcePin)
-                let destinationPin = CustomPin(pinTitle: targetAddress, pinSubTitle: "", location: destinationLocation)
-                self.mapView.addAnnotation(destinationPin)
+                //        let sourceLocation = CLLocationCoordinate2D(latitude:39.173209 , longitude: -94.593933)
+                let sourceLocation = cllocation.coordinate
                 
-                let sourcePlaceMark = MKPlacemark(coordinate: sourceLocation)
-                let destinationPlaceMark = MKPlacemark(coordinate: destinationLocation)
-                
-                let directionRequest = MKDirectionsRequest()
-                directionRequest.source = MKMapItem(placemark: sourcePlaceMark)
-                directionRequest.destination = MKMapItem(placemark: destinationPlaceMark)
-                directionRequest.transportType = .automobile
-                
-                let directions = MKDirections(request: directionRequest)
-                directions.calculate { (response, error) in
-                    guard let directionResonse = response else {
-                        if let error = error {
-                            print("we have error getting directions==\(error.localizedDescription)")
+                let targetAddress = UserDefaults.standard.string(forKey: "targetAddress") ?? "渋谷駅"
+                Locator.location(
+                    fromAddress: targetAddress,
+                    onSuccess: { cllocations in
+                        print("Location:\(cllocation.debugDescription)")
+                        
+                        //                let destinationLocation = CLLocationCoordinate2D(latitude:38.643172 , longitude: -90.177429)
+                        guard let destinationLocation = cllocations.first?.coordinates else {
+                            print("targetAddress not found")
+                            return
                         }
-                        return
-                    }
-                    
-                    let route = directionResonse.routes[0]
-                    self.mapView.add(route.polyline, level: .aboveRoads)
-                    
-                    let rect = route.polyline.boundingMapRect
-                    self.mapView.setRegion(MKCoordinateRegionForMapRect(rect), animated: true)
+                        
+                        //                let sourcePin = CustomPin(pinTitle: "現在地", pinSubTitle: "", location: sourceLocation)
+                        //                self.mapView.addAnnotation(sourcePin)
+                        let destinationPin = CustomPin(pinTitle: targetAddress, pinSubTitle: "", location: destinationLocation)
+                        self.mapView.addAnnotation(destinationPin)
+                        
+                        let sourcePlaceMark = MKPlacemark(coordinate: sourceLocation)
+                        let destinationPlaceMark = MKPlacemark(coordinate: destinationLocation)
+                        
+                        let directionRequest = MKDirectionsRequest()
+                        directionRequest.source = MKMapItem(placemark: sourcePlaceMark)
+                        directionRequest.destination = MKMapItem(placemark: destinationPlaceMark)
+                        directionRequest.transportType = .automobile
+                        
+                        let directions = MKDirections(request: directionRequest)
+                        directions.calculate { (response, error) in
+                            guard let directionResonse = response else {
+                                if let error = error {
+                                    print("we have error getting directions==\(error.localizedDescription)")
+                                }
+                                return
+                            }
+                            
+                            let route = directionResonse.routes[0]
+                            self.mapView.add(route.polyline, level: .aboveRoads)
+                            
+                            let rect = route.polyline.boundingMapRect
+                            self.mapView.setRegion(MKCoordinateRegionForMapRect(rect), animated: true)
+                        }
+                        
+                        //set delegate for mapview
+                        self.mapView.delegate = self
+                },
+                    onFail: { locationError in
+                        print("Failed to get location: \(locationError).")
                 }
-                
-                //set delegate for mapview
-                self.mapView.delegate = self
+                )
         },
-            onFail: { locationError in
-                print("Failed to get location: \(locationError).")
+            onFail: { locationError, cllocation in
+                print("Failed to get location: \(locationError). Location:\(cllocation.debugDescription)")
         }
         )
     }
